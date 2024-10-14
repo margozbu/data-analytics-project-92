c9f855cc87cd9ba45979ddf4bae9844f86f8206d
margozbu <margueriteposte@gmail.com>
2024-10-14 22:08:14 +0300

    Update queries.sql
    add owest_average_income query

--- queries.sql
+++ queries.sql
@@ -13,3 +13,18 @@ left join sales s on e.employee_id = s.sales_person_id
 left join products p on s.product_id = p.product_id
 group by e.first_name, e.last_name
 order by income desc nulls last limit 10;
+
+-- определяем информацию о продавцах, чья средняя выручка за сделку меньше средней выручки за сделку по всем продавцам
+select
+	concat_ws(' ', e.first_name, e.last_name) as seller,
+	trunc(avg(s.quantity * p.price)) as average_income
+from employees e
+left join sales s on e.employee_id = s.sales_person_id
+left join products p on s.product_id = p.product_id
+group by e.first_name, e.last_name
+having trunc(avg(s.quantity * p.price)) < (
+	select
+		trunc(avg(s2.quantity * p2.price))
+	from sales s2
+	inner join products p2 on s2.product_id = p2.product_id)
+order by average_income ASC;
